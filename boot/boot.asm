3d71120e41d319f1ca8a8276a28bd9954bb49d1b
yasaswiniii0822 <yasu822@gmail.com>
2026-03-10 17:26:49 +0000

    keyboard input can be taken(basic)

--- boot/boot.asm
+++ boot/boot.asm
@@ -5,8 +5,15 @@ start:
     mov si, logo
     call print_string
 
-hang:
-    jmp hang
+input_loop:
+
+    mov ah, 0x00
+    int 0x16
+
+    mov ah, 0x0e
+    int 0x10
+
+    jmp input_loop
 
 print_string:
 .next_char:
@@ -29,7 +36,8 @@ logo db 13,10
      db 13,10
      db "  Loading kernel...",13,10
      db "  Starting services...",13,10
-     db 0
+     db 13,10
+     db "Aurora >",0
 
 times 510-($-$$) db 0
 dw 0xaa55
