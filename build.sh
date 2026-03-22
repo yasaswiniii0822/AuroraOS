84c792c2ca7411f224f46f7fbb68a4d854d56f20
yasaswiniii0822 <yasu822@gmail.com>
2026-03-22 15:50:19 +0000

    AuroraOS version V0.1

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "Building AuroraOS..."
 
 nasm -f bin boot.asm -o boot.bin
@@ -15,13 +14,10 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Creating disk image..."
-
 rm -f os.img
 dd if=/dev/zero of=os.img bs=512 count=2880 2>/dev/null
-
 dd if=boot.bin of=os.img bs=512 count=1 conv=notrunc 2>/dev/null
 dd if=kernel.bin of=os.img bs=512 seek=1 conv=notrunc 2>/dev/null
 
-echo "Starting AuroraOS..."
-
+echo "Done! Starting AuroraOS..."
 qemu-system-x86_64 -drive format=raw,file=os.img
